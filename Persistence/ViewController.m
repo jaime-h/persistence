ecea51f1b0ff61e8898514d630f058bdb1346f0c
Jaime Hernandez <jaime.e.hernandez@gmail.com>
2015-01-14 15:59:43 -0600

    First Commit of Test App
    Trying out writing info to PLIST
    Create GUI

--- Persistence/ViewController.m
+++ Persistence/ViewController.m
@@ -5,6 +5,9 @@
 //  Created by Jaime Hernandez on 1/14/15.
 //  Copyright (c) 2015 Jaime Hernandez. All rights reserved.
 //
+//  From the follwoing tutorial on persistence
+//  http://www.binpress.com/tutorial/learn-objectivec-building-an-app-part-10-basic-data-persistence/103
+//
 
 #import "ViewController.h"
 
