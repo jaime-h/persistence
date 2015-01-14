0da7513789661dae18c195d9bba22a4c62f3bd16
Jaime Hernandez <jaime.e.hernandez@gmail.com>
2015-01-14 15:50:57 -0600

    Initial Commit

---
+++ Persistence/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Persistence
+//
+//  Created by Jaime Hernandez on 1/14/15.
+//  Copyright (c) 2015 Jaime Hernandez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Persistence/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Persistence
+//
+//  Created by Jaime Hernandez on 1/14/15.
+//  Copyright (c) 2015 Jaime Hernandez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
